f58694358c20968b8e407a74d008d415aca6105e
ddsdAlp <alp35cetintas@gmail.com>
2024-11-18 22:34:58 +0300

    2nd Update -rewamped the FriendRequest system
    -deleted all things related to FriendRequest and implemented them in User

--- src/main/java/org/howudoin/controller/UserController.java
+++ src/main/java/org/howudoin/controller/UserController.java
@@ -5,6 +5,7 @@ import org.howudoin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -22,4 +23,25 @@ public class UserController {
     public User loginUser(@RequestBody User loginRequest){
         return userService.loginUser(loginRequest.getEmail(), loginRequest.getPassword());
     }
+
+    @PostMapping("/friends/add")
+    public String sendFriendRequest(@RequestParam String senderEmail, @RequestParam String receiverEmail){
+        return userService.sendRequest(senderEmail,receiverEmail);
+    }
+
+    @PostMapping("/friends/accept")
+    public String respondToFriendRequest(@RequestParam String senderEmail, @RequestParam String receiverEmail){
+        return  userService.acceptRequest(senderEmail, receiverEmail);
+    }
+
+    @GetMapping("/friends")
+    public List<String> showFriends(@RequestParam String email){
+        return userService.showFriends(email);
+    }
+
+    @GetMapping("/show")
+    public List<User> showUsers(){
+        return userService.showAllUsers();
+    }
+
 }
--- src/main/java/org/howudoin/model/User.java
+++ src/main/java/org/howudoin/model/User.java
@@ -2,16 +2,24 @@ package org.howudoin.model;
 
 import org.springframework.data.annotation.Id;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
 
-    private String name;
-    private String lastName;
-    private String email;
-    private String password;
+    @Id
+    private String Id;
+    private String name = "name";
+    private String lastName = "lastname";
+    private String email = "email";
+    private String password = "password";
 
-    private List<String> friends; // List of user Emails
+    private List<String> friends = new ArrayList<>(); // List of user Emails who are friends of this user
+    private List<String> friendRequests = new ArrayList<>();; //List of user Emails who have sent a friend request to user
+
+    public String getName() {
+        return name;
+    }
 
     public String getEmail() {
         return email;
@@ -22,5 +30,29 @@ public class User {
 
     public List<String> getFriends(){
         return friends;
+    }
+
+    public List<String> getFriendRequests(){
+        return friendRequests;
+    }
+
+    public void setFriendRequests(String email){
+        friendRequests.add(email);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
